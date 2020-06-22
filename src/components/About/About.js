10bf7d82f4efbc4ac7fe93aa0dafe33a9bffd2cd
Aegir Boissel <aegir.boissel@docapost-agility.fr>
2020-06-22 14:52:43 +0200

    retouche page About ajout Montserrat

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,35 +1,34 @@
 import React from "react";
-import { Grid, Header, Segment } from "semantic-ui-react";
 import Profile from "./Profile";
 
 const About = () => (
   <div>
-    <Header as="h3" divider>
-      About
-    </Header>
-    <Segment padded="very">
-      <Grid relaxed columns={4}>
-        <Grid.Column>
-          <Profile
-            name="Vincent Hirtz"
-            img="https://avatars3.githubusercontent.com/u/31092476?s=460&u=1b18f3aa32d4d9ec08bf359b175c613821de277a&v=4"
-            work="Dev front end"
-            github="https://github.com/vincenthirtz"
-            date="Creator in June 2020"
-            country="france"
-          />
-        </Grid.Column>
-        <Grid.Column>
-          <Profile
-            name="Jack Sparrow"
-            img="https://randomuser.me/api/portraits/lego/5.jpg"
-            work="Working on ..."
-            github="https://github.com/"
-            date="Contact us to join"
-          />
-        </Grid.Column>
-      </Grid>
-    </Segment>
+    <h3>About</h3>
+    <div className="profilHolder">
+      <Profile
+        name="Vincent Hirtz"
+        img="https://avatars3.githubusercontent.com/u/31092476?s=460&u=1b18f3aa32d4d9ec08bf359b175c613821de277a&v=4"
+        work="Dev front end"
+        github="https://github.com/vincenthirtz"
+        date="Creator in June 2020"
+        country="france"
+      />
+      <Profile
+        name="Jack Sparrow"
+        img="https://randomuser.me/api/portraits/lego/5.jpg"
+        work="Working on ..."
+        github="https://github.com/"
+        date="Contact us to join"
+      />
+      <Profile
+        name="Bébou"
+        img="https://avatars0.githubusercontent.com/u/57497391?s=460&u=09117532f789791ca04db290c9d63b113c82611f&v=4"
+        work="I know kung fu"
+        github="https://github.com/ertrasLuapnaej"
+        date="Creator in June 2020"
+        country="france"
+      />
+    </div>
   </div>
 );
 
