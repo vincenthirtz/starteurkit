b7c99e7d7d7e50fb4a610b58a2d7d68707873877
Aegir Boissel <aegir.boissel@docapost-agility.fr>
2020-06-23 17:03:14 +0200

    le menu descend ... joie

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -10,6 +10,7 @@ const Layout = (props) => {
   const [OpenMenu, setOpenMenu] = OpenMenuHook.useOpenMenu();
   return (
     <>
+      <span id="haut_de_page" />
       <button type="button" className={`burgerMenu ${OpenMenu ? "open" : ""}`} onClick={() => setOpenMenu(!OpenMenu)}>
         <div>
           <span className="bun1" />
@@ -17,8 +18,12 @@ const Layout = (props) => {
           <span className="bun2" />
         </div>
       </button>
-      <Sidebar>{children}</Sidebar>
-
+      <Sidebar>
+        <a className="travelTop" href="#haut_de_page">
+          get to the top mohefoca
+        </a>
+        {children}
+      </Sidebar>
       <Footer />
     </>
   );
