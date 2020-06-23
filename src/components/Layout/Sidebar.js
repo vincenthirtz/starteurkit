970fb5fb9b256eb409b1ba68b9785ef264bd9ff1
Vincent Hirtz <vincent.hirtz@smartpanda.com>
2020-06-23 09:18:57 +0200

    fix eslint problems

--- src/components/Layout/Sidebar.js
+++ src/components/Layout/Sidebar.js
@@ -5,44 +5,43 @@ import { useTranslation } from "react-i18next";
 import { navigate, getWorkingPath } from "hookrouter";
 import { Flag } from "semantic-ui-react";
 import Header from "./Header";
-import OutsideAlerter from './menuWrapper';
+import OutsideAlerter from "./menuWrapper";
 
 import OpenMenuHook from "../customHook";
 
-
 const SidebarComponent = (props) => {
-  const { children} = props;
+  const { children } = props;
   const { t } = useTranslation();
 
   const setLangFR = () => i18n.changeLanguage("fr-FR");
 
   const setLangEN = () => i18n.changeLanguage("en-US");
 
-  const [OpenMenu, setOpenMenu] = OpenMenuHook.useOpenMenu();
+  const [OpenMenu] = OpenMenuHook.useOpenMenu();
   return (
     <>
       <div className="headerHome">
-    <OutsideAlerter>
-        <div className={`topMenu ${OpenMenu ? "" : "hidden"}`}>
-          <a onClick={() => navigate("/")} active={getWorkingPath() === "/"}>
-            {t("Home")}
-          </a>
-          <a onClick={() => navigate("/features")} active={getWorkingPath() === "/features"}>
-            {t("Features")}
-          </a>
-          <a onClick={() => navigate("/about")} active={getWorkingPath() === "/about"}>
-            {t("About")}
-          </a>
-          <a onClick={() => navigate("/contact")} active={getWorkingPath() === "/contact"}>
-            {t("Contact")}
-          </a>
-          <Flag name="france" onClick={setLangFR} className="flag" />
-          <Flag name="uk" onClick={setLangEN} className="flag" />
-        </div>
-      </OutsideAlerter>
+        <OutsideAlerter>
+          <div className={`topMenu ${OpenMenu ? "" : "hidden"}`}>
+            <a onClick={() => navigate("/")} active={getWorkingPath() === "/"}>
+              {t("Home")}
+            </a>
+            <a onClick={() => navigate("/features")} active={getWorkingPath() === "/features"}>
+              {t("Features")}
+            </a>
+            <a onClick={() => navigate("/about")} active={getWorkingPath() === "/about"}>
+              {t("About")}
+            </a>
+            <a onClick={() => navigate("/contact")} active={getWorkingPath() === "/contact"}>
+              {t("Contact")}
+            </a>
+            <Flag name="france" onClick={setLangFR} className="flag" />
+            <Flag name="uk" onClick={setLangEN} className="flag" />
+          </div>
+        </OutsideAlerter>
         {getWorkingPath() === "/" && <Header />}
       </div>
-      <div style={{marginTop:'4rem'}}>{children}</div>
+      <div style={{ marginTop: "4rem" }}>{children}</div>
     </>
   );
 };
@@ -51,5 +50,4 @@ export default SidebarComponent;
 
 SidebarComponent.propTypes = {
   children: PropTypes.element.isRequired,
-  open: PropTypes.element.isRequired,
 };
