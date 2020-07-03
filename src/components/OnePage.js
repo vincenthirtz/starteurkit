ee31c3da7fafb762f385db21712c0e4ceee95e80
aegir.boissel <aegir.boissel@docapost-agility.fr>
2020-07-03 18:08:00 +0200

    avancée du mode one page

--- src/components/OnePage.js
+++ src/components/OnePage.js
@@ -4,20 +4,30 @@ import Features from "./Features";
 import Contact from "./Contact";
 import About from "./About/About";
 import Footer from "./Layout/Footer";
+import "../style/app.scss";
 
 const OnePage = () => {
   return (
     <>
-      <a href="#contactOne">Contact</a>
-      <>
-        <Home id="homeOne" />
-        <Features id="featuresOne" />
-        <About id="aboutOne" />
-
+      <ul id="onePageMenu">
+        <a href="#featuresOne">Features</a>
+        <a href="#aboutOne">About</a>
+        <a href="#contactOne">Contact</a>
+      </ul>
+      <Home id="homeOne" />
+      <div>
+        <a id="featuresOne" />
+        <Features />
+      </div>
+      <div>
+        <a id="aboutOne" />
+        <About />
+      </div>
+      <div>
+        <a id="contactOne" />
         <Contact />
-        <Footer />
-        <a id="contactOne">pef</a>
-      </>
+      </div>
+      <Footer />
     </>
   );
 };
