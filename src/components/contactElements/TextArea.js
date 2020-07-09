510912d3d51175e441759a116ed4ad835b45a9f3
Aegir Boissel <aegir.boissel@docapost-agility.fr>
2020-07-09 16:48:46 +0200

    oskor

--- src/components/contactElements/TextArea.js
+++ src/components/contactElements/TextArea.js
@@ -1,26 +1,32 @@
 import React from "react";
-
-
+import PropTypes from "prop-types";
 
 export const InputField = (props) => {
-    const { label } = props;
+  const { label } = props;
 
-    return (
-        <>
-            <span className="formTitle" >{label}</span>
-            <input></input>
-        </>
-    );
+  return (
+    <>
+      <span className="formTitle">{label}</span>
+      <input />
+    </>
+  );
 };
 
 export const TextArea = (props) => {
-    const { label } = props;
+  const { label } = props;
 
-    return (
-        <>
-            <span className="formTitle" >{label}</span>
-            <textarea label={label} placeholder={`Message`} required ></textarea >
-        </>
-    );
+  return (
+    <>
+      <span className="formTitle">{label}</span>
+      <textarea label={label} placeholder="Message" />
+    </>
+  );
 };
 
+TextArea.propTypes = {
+  label: PropTypes.string.isRequired,
+};
+
+InputField.propTypes = {
+  label: PropTypes.string.isRequired,
+};
