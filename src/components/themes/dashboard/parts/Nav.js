7580b642cbd2eebb7e350ef003112b64fdb8c008
Aegir Boissel <aegir.boissel@docapost-agility.fr>
2020-07-29 17:01:17 +0200

    rono tient dans sont image

--- src/components/themes/dashboard/parts/Nav.js
+++ src/components/themes/dashboard/parts/Nav.js
@@ -1,16 +1,18 @@
 import React from "react";
 
 const Nav = (props) => {
-    const {navStuff}=props
+    const { navStuff } = props
 
     const menuInput = navStuff.split(",");
     const menu = menuInput.map(m =>
-            <ul>
-                <a>{m}</a>
-            </ul>
-       )
-console.log('menu ',menu)
-  return <div>{menu}</div>
+        <a>{m}</a>
+    )
+    console.log('menu ', menu)
+    return <div>
+        <ul>
+            {menu}
+        </ul>
+    </div>
 };
 
 export default Nav;
