3d704644b6b4ca3949392614068ee6baf204154b
Aegir Boissel <aegir.boissel@docapost-agility.fr>
2020-08-05 09:32:05 +0200

    fix(eslint): some problems

--- src/components/themes/dashboard/parts/Nav.js
+++ src/components/themes/dashboard/parts/Nav.js
@@ -1,70 +1,83 @@
-import React, { useEffect, useRef} from "react";
+import React, { useEffect, useRef } from "react";
+import PropTypes from "prop-types";
 import SubMenuOpen from "../../../subMenuCustom";
 
 const Nav = (props) => {
-    const { navStuff, optionsNav } = props;
-    const { align, reference } = optionsNav;
-    const [, setOpenMenuGreen] = SubMenuOpen.useSubMenuOpen();
-    const node = useRef();
-    const menuInput = navStuff.split(",");
-    const menu = menuInput.map(m =>
-        <span>{m}</span>
-    );
+  const { navStuff, optionsNav } = props;
+  const { align, reference } = optionsNav;
+  const [, setOpenMenuGreen] = SubMenuOpen.useSubMenuOpen();
+  const node = useRef();
+  const menuInput = navStuff.split(",");
+  const menu = menuInput.map((m) => <span key={m}>{m}</span>);
 
-    let navPosition = "";
-    let navClass = "";
+  let navPosition = {};
+  let navClass = "";
 
-        switch (align) {
-            case 'left':
-                navClass='navLeft'; 
-                navPosition = {
-                    width: "100%",
-                    height:"100%"
-                };
-                break;
-            case 'center':
-                navClass='navCenter'; 
-                navPosition = {
-                    width: "100%",
-                };
-                break;
-            case 'right':
-                navClass='navRight'; 
-                navPosition={
-                    width: "100%",
-                };
-                break;
-        };
-    
+  switch (align) {
+    case "left":
+      navClass = "navLeft";
+      navPosition = {
+        width: "100%",
+        height: "100%",
+      };
+      break;
+    case "center":
+      navClass = "navCenter";
+      navPosition = {
+        width: "100%",
+      };
+      break;
+    case "right":
+      navClass = "navRight";
+      navPosition = {
+        width: "100%",
+      };
+      break;
+    default:
+      navClass = "navLeft";
+      navPosition = {
+        width: "100%",
+        height: "100%",
+      };
+      break;
+  }
 
-    useEffect(() => {
+  useEffect(() => {
+    const handleClick = (e) => {
+      if (node.current.contains(e.target)) {
+        return;
+      }
 
-        const handleClick = (e) => {
-            if (node.current.contains(e.target)) {
-                return;
-            }
+      if (!!reference && !!reference.current && !reference.current.contains(e.target)) {
+        setOpenMenuGreen(false);
+      }
+    };
 
-            if (!!reference && !!reference.current && !reference.current.contains(e.target)) {
-                setOpenMenuGreen(false);
-            }
-        }
+    document.addEventListener("mousedown", handleClick);
+    return () => {
+      document.removeEventListener("mousedown", handleClick);
+    };
+  }, []);
 
-        document.addEventListener("mousedown", handleClick);
-        return () => {
-            document.removeEventListener("mousedown", handleClick);
-        };
-    }, [])
-
-    return (
-        <div ref={node} 
-        style={navPosition} 
-        className={navClass}
-        >
-            <ul >
-                {menu}
-            </ul>
-        </div>
-    )
+  return (
+    <div ref={node} style={navPosition} className={navClass}>
+      <ul>{menu}</ul>
+    </div>
+  );
 };
 
 export default Nav;
+
+Nav.propTypes = {
+  navStuff: PropTypes.string,
+  optionsNav: PropTypes.shape({
+    align: PropTypes.string,
+    colorGeneral: PropTypes.string,
+    reference: PropTypes.string,
+  }),
+};
+
+Nav.defaultProps = {
+  optionsNav: {},
+  navStuff: "",
+};
